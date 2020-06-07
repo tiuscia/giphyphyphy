db8470b81d1d50607b4d64b993399dd17fe48f2b
katiazanotti <tiuscia.work@gmail.com>
2020-06-07 22:02:34 +0200

    show no results

--- src/layouts/SearchLayout/SearchLayout.jsx
+++ src/layouts/SearchLayout/SearchLayout.jsx
@@ -5,21 +5,29 @@ function SearchLayout() {
   const [searchQuery, setSearchQuery] = useState('');
   const [apiSearchQuery, setApiSearchQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     
     async function fetchData(){
+        setError(false);
         try{
             const fetchedData = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${apiSearchQuery}&limit=25&offset=0&rating=G&lang=en`);
             const jsonData = await fetchedData.json();
             console.log('json', jsonData);
-            setResults(
-                jsonData.data.map(item=>{
-                    return item.images.preview.mp4;
-                })
-            );
+            if(jsonData.data.length === 0){
+                setError(true);
+            } else {
+                setResults(
+                    jsonData.data.map(item=>{
+                        return item.images.preview.mp4;
+                    })
+                );
+            }
+            
         }
         catch(e){
+            setError(true);
             console.log(e)
         }
     };
@@ -45,13 +53,14 @@ function SearchLayout() {
             placeholder="Search c'mon"
             type="text"
             id="search" />
-            <label for="search">Search</label>
+            <label for="search">search</label>
                 </div>
             
             <button type="submit">searchphyphy</button>
         </form>
         {results.length > 0 && <h2>here are your giphyphyphy</h2>}
         <div className="search-layout__videos">
+            {error && <span>Sorry. No results.</span>}
         {results.map(item => (
             <video key={item} autoPlay loop src={item}/>
         ))}
